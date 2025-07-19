b158c17e569e66e0636809d650ef138cef2a1b84
Jonas-Hoang <bangbang3951@gmail.com>
2025-07-19 22:32:38 +0700

    Initial Commit

---
+++ DemoLightDE/DemoLightDEApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoLightDEApp.swift
+//  DemoLightDE
+//
+//  Created by Jonas Hoang on 19/7/25.
+//
+
+import SwiftUI
+
+@main
+struct DemoLightDEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
